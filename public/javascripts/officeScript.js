88335a1b27d8baa05cde925a360d4f479383b96a
tjasasinkovec <tsinkovec@icloud.com>
2020-11-08 03:35:14 +0100

    Enabled changing graphs

--- public/javascripts/officeScript.js
+++ public/javascripts/officeScript.js
@@ -13,10 +13,56 @@ function pridobiPodatke(callback){
     return " Hahaha"
 }
 
-function pridobiObcutke(){
-    let res = $.ajax({type: "POST", url: '/feeling/getAll', async: false});
+function pridobiPodatkeHumidity(callback){
+    let res = $.ajax({type: "POST", url: '/data/getData', async: false})
+    let data;
     if(res.responseJSON){
-        let data = res.responseJSON;
-        console.log(data);
+        data = res.responseJSON;
+        dict = []
+        for (var key in data) {
+            dict.push({"date": data[key].time.date.concat(" ",data[key].time.hour.toString(),":",data[key].time.minute.toString()), "value": data[key].humidity })
+        }
+        return dict;
+    }
+    console.log(res);
+    return " Hahaha"
+}
+
+function pridobiPodatkeBrightness(callback){
+    let res = $.ajax({type: "POST", url: '/data/getData', async: false})
+    let data;
+    if(res.responseJSON){
+        data = res.responseJSON;
+        dict = []
+        for (var key in data) {
+            dict.push({"date": data[key].time.date.concat(" ",data[key].time.hour.toString(),":",data[key].time.minute.toString()), "value": data[key].brightness })
+        }
+        return dict;
+    }
+    console.log(res);
+    return " Hahaha"
+}
+
+function pridobiPodatkeNoise(callback){
+    let res = $.ajax({type: "POST", url: '/data/getData', async: false})
+    let data;
+    if(res.responseJSON){
+        data = res.responseJSON;
+        dict = []
+        for (var key in data) {
+            dict.push({"date": data[key].time.date.concat(" ",data[key].time.hour.toString(),":",data[key].time.minute.toString()), "value": data[key].noise })
+        }
+        return dict;
+    }
+    console.log(res);
+    return " Hahaha"
+}
+
+function pridobiObcutke(){
+    let res = $.ajax({type: "POST", url: '/data/getAll', async: false})
+    let data;
+    if(res.responseJSON){
+        data = res.responseJSON;
+        console.log(data)
     }
 }
